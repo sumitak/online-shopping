5c6e6fce98dcd62ade6b9cd8746e623d420a8d23
sumitak <k.sumita@gmail.com>
2025-12-16 03:02:20 +0530

    Added integration test case

--- product-service/src/test/java/com/techno/practice/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/techno/practice/product/ProductServiceApplicationTests.java
@@ -1,15 +1,61 @@
 package com.techno.practice.product;
 
+import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
+import org.hamcrest.Matchers;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+// it will start the application context with a random port and include the Testcontainers configuration for MongoDB
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ProductServiceApplication.class)
 class ProductServiceApplicationTests {
 
+    /**
+     * The MongoDBContainer instance managed by Testcontainers.
+     * ServiceConnection annotation ensures that Spring Boot connects to this container for MongoDB operations.
+     */
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+    }
+
 	@Test
-	void contextLoads() {
-	}
+	void shouldCreateProduct() {
+        String productRequest = """
+                {
+                      "name":"Java25",
+                      "price":401,
+                      "description": "First Ahead Book1"
+                  }
+                """;
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(productRequest)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id",  Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Java25"))
+                .body("description",  Matchers.equalTo("First Ahead Book1"))
+                .body("price",  Matchers.equalTo(401));
+    }
+
+
 
 }
